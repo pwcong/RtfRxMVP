929dce19388dce2687694dec580550836bd076d4
pwcong <pwcong@foxmail.com>
2016-08-31 00:35:47 +0800

    添加笑话选项功能。

--- app/src/main/java/me/pwcong/rtfrxmvp/adapter/BasePagerAdapter.java
+++ app/src/main/java/me/pwcong/rtfrxmvp/adapter/BasePagerAdapter.java
@@ -11,6 +11,7 @@ import java.util.List;
  */
 public abstract class BasePagerAdapter<T> extends FragmentPagerAdapter {
 
+
     List<T> list;
 
     public BasePagerAdapter(FragmentManager fm, List<T> list) {
