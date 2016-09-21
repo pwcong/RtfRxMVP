095ab895542d2e0ab79c997f477a27cf5d8c623d
Pwcong <pwcong@foxmail.com>
2016-09-21 23:01:46 +0800

    增加夜晚自动更换夜间皮肤功能。

--- app/src/main/java/me/pwcong/rtfrxmvp/App.java
+++ app/src/main/java/me/pwcong/rtfrxmvp/App.java
@@ -1,6 +1,7 @@
 package me.pwcong.rtfrxmvp;
 
 import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
 
 /**
  * Created by pwcong on 2016/8/19.
@@ -8,6 +9,12 @@ import android.app.Application;
 public class App extends Application {
 
     public static App instance =null;
+
+    static {
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+
+    }
 
     @Override
     public void onCreate() {
